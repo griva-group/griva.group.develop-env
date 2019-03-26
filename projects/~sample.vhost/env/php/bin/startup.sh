208b9e57449faa90be82791f3bb3d04d13a269f7
Vasily <v.grigorev@runway-agency.ru>
2019-03-26 08:30:47 +0300

    chor: подготовка к релизу версии 0.3.0
    Разработана новая упрощенная схема построения виртуальных хостов.

---
+++ projects/~sample.vhost/env/php/bin/startup.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+## START SECTION -- LIVE FUNCTIONS
+GGDDE_NGINX_CONF="/var/www/$(ls /var/www | head -1)/env/php/etc"
+
+_activate_nginx_host() {
+    if [ -f "$GGDDE_NGINX_CONF/nginx.conf" ]; then
+        envsubst "`printf '${%s} ' $(bash -c "compgen -A variable")`" < "$GGDDE_NGINX_CONF/nginx.conf" > "$GGDDE_NGINX_CONF/live.nginx.conf"
+    else
+        echo "NOTICE: file $GGDDE_NGINX_CONF/nginx.conf not found!"
+    fi
+}
+_disable_nginx_host() {
+    if [ -f "$GGDDE_NGINX_CONF/live.nginx.conf" ]; then
+        rm "$GGDDE_NGINX_CONF/live.nginx.conf"
+    fi
+}
+
+_activate_nginx_host
+trap _disable_nginx_host INT TERM
+## END SECTION -- LIVE FUNCTIONS
+
+# Create symlink for vhost folder
+ln -sf /var/www/vhost /var/www/${PHP_DIR}
+chown www-data:www-data /var/www/${PHP_DIR}
+
+# Change php-fpm config dir
+cd /etc/php/${PHP_VERSION}/fpm/
+
+# Copy php-fpm configs
+rm php.ini      && ln -s ../../all/fpm/php.ini      php.ini
+rm php-fpm.conf && ln -s ../../all/fpm/php-fpm.conf php-fpm.conf
+
+# Generate php-fpm pools config
+cd pool.d/ && rm -r ./*
+for poolfile in `find /etc/php/all/fpm/pool.d/ -type f`
+do
+    envsubst "`printf '${%s} ' $(bash -c "compgen -A variable")`" < $poolfile > `basename $poolfile`
+done
+
+# Running server
+/usr/sbin/php-fpm${PHP_VERSION} --nodaemonize &
+
+# For realy gracefull stoping container
+wait $!
