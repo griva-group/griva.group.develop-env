98ac7ca741cd00a6b9388d5ee26b835ef7a38cc5
Vasily <v.grigorev@runway-agency.ru>
2019-06-05 11:48:41 +0300

    perf (php,mysql): обновлен скрипт docker-compose.yml
    Конфигурационный файл docker-compose.yml обновлен и приближен к docker-подобному образу системы, когда по максимуму используются уже готовые решения.
    
    Так же, контейнер php получил пример конфигурации nginx (reverse proxy) + apache2 (backend)
    
    Контейнер mysql по умолчанию лиует раздел данных в виртуальный файловый раздел для более эффективной и быстрой работы.

--- projects/~sample.vhost/env/php/bin/startup.sh
+++ projects/~sample.vhost/env/php/bin/startup.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
-
 ## START SECTION -- LIVE FUNCTIONS
-GGDDE_NGINX_CONF="/var/www/$(ls /var/www | head -1)/env/php/etc"
-
+GGDDE_NGINX_CONF="/var/www/vhost/env/php/etc/nginx"
 _activate_nginx_host() {
     if [ -f "$GGDDE_NGINX_CONF/nginx.conf" ]; then
-        envsubst "`printf '${%s} ' $(bash -c "compgen -A variable")`" < "$GGDDE_NGINX_CONF/nginx.conf" > "$GGDDE_NGINX_CONF/live.nginx.conf"
+        envsubst < "$GGDDE_NGINX_CONF/nginx.conf" > "$GGDDE_NGINX_CONF/live.nginx.conf"
     else
         echo "NOTICE: file $GGDDE_NGINX_CONF/nginx.conf not found!"
     fi
@@ -15,31 +13,26 @@ _disable_nginx_host() {
         rm "$GGDDE_NGINX_CONF/live.nginx.conf"
     fi
 }
-
 _activate_nginx_host
 trap _disable_nginx_host INT TERM
 ## END SECTION -- LIVE FUNCTIONS
 
-# Create symlink for vhost folder
-ln -sfnF /var/www/vhost /var/www/${PHP_DIR}
-chown www-data:www-data /var/www/${PHP_DIR}
+echo ${GGDDE_NGINX_CONF}
 
-# Change php-fpm config dir
-cd /etc/php/${PHP_VERSION}/fpm/
+# Add host loop to host machine
+#echo "$(/sbin/ip route|awk '/default/ { print $3 }')  ${PHP_HOST}" >> /etc/hosts
 
-# Copy php-fpm configs
-rm php.ini      && ln -s ../../all/fpm/php.ini      php.ini
-rm php-fpm.conf && ln -s ../../all/fpm/php-fpm.conf php-fpm.conf
+# Create symlink for vhost folder and reset rights
+cd /var/www && ln -sfnF vhost ${PHP_DIR}
+chown -R www-data:www-data /var/www
 
-# Generate php-fpm pools config
-cd pool.d/ && rm -r ./*
-for poolfile in `find /etc/php/all/fpm/pool.d/ -type f`
+for poolfile in `find /usr/local/etc/php-fpm.d/ -type f`
 do
-    envsubst "`printf '${%s} ' $(bash -c "compgen -A variable")`" < $poolfile > `basename $poolfile`
+     envsubst < $poolfile > `basename $poolfile`
 done
 
 # Running server
-/usr/sbin/php-fpm${PHP_VERSION} --nodaemonize &
+php-fpm
 
 # For realy gracefull stoping container
 wait $!
